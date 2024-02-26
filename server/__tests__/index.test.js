1c471c764370deb4a93e872690e7336e275b9838
Faith Collison <faithcollison@yahoo.co.uk>
2024-02-26 12:51:19 +0000

    add env variables for testing, change tests to get

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -1,5 +1,5 @@
-const app = require('../index')
-const request = require('supertest')
+const app = require("../index");
+const request = require("supertest");
 
 let server;
 
@@ -7,88 +7,73 @@ beforeAll(() => {
   server = app.listen(9090);
 });
 
-afterAll(done => {
+afterAll((done) => {
   server.close(done);
 });
 
-describe('POST /data', () => {
-    test('POST : 200 sends an object of vehicle details to client', async () => {
-        const regPlate = {
-            "registrationNumber": "AA19AAA"
-        }
-        const response = await request(app)
-        .post("/data")
-        .send(regPlate)
-
-        expect(response.statusCode).toBe(200)
-        expect(response.body).toEqual(
-            expect.objectContaining({
-                "registrationNumber": "AA19AAA",
-                "artEndDate": "2025-03-30",
-                "co2Emissions": 300,
-                "engineCapacity": 2000,
-                "euroStatus": "EURO1",
-                "markedForExport": false,
-                "fuelType": "PETROL",
-                "motStatus": "No details held by DVLA",
-                "revenueWeight": 0,
-                "colour": "RED",
-                "make": "FORD",
-                "typeApproval": "M1",
-                "yearOfManufacture": 2019,
-                "taxStatus": "Taxed",
-                "dateOfLastV5CIssued": "2019-05-20",
-                "wheelplan": "2 AXLE RIGID BODY",
-                "monthOfFirstDvlaRegistration": "2019-03",
-                "monthOfFirstRegistration": "2019-03",
-                "realDrivingEmissions": "1"
-              }
-              )
-        )
+describe("POST /data", () => {
+  test("GET : 200 sends an object of vehicle details to client", async () => {
+    const regPlate = "AA19AAA";
+    const response = await request(app).get(
+      `/api/lookup?registrationNumber=${regPlate}`
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        registrationNumber: "AA19AAA",
+        artEndDate: "2025-03-30",
+        co2Emissions: 300,
+        engineCapacity: 2000,
+        euroStatus: "EURO1",
+        markedForExport: false,
+        fuelType: "PETROL",
+        motStatus: "No details held by DVLA",
+        revenueWeight: 0,
+        colour: "RED",
+        make: "FORD",
+        typeApproval: "M1",
+        yearOfManufacture: 2019,
+        taxStatus: "Taxed",
+        dateOfLastV5CIssued: "2019-05-20",
+        wheelplan: "2 AXLE RIGID BODY",
+        monthOfFirstDvlaRegistration: "2019-03",
+        monthOfFirstRegistration: "2019-03",
+        realDrivingEmissions: "1",
+      })
+    );
+  });
+  test("GET : 400 sends appropriate status and error message when invalid request made", async () => {
+    const regPlate = "ER19BAD";
+    const response = await request(app).get(
+      `/api/lookup?registrationNumber=${regPlate}`
+    );
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({
+      errors: [
+        {
+          status: "400",
+          code: "400",
+          title: "Bad Request",
+          detail: "Invalid format for field - vehicle registration number",
+        },
+      ],
     });
-    test('POST : 400 sends appropriate status and error message when invalid request made',  async () => {
-        const regPlate = {
-            "registrationNumber": "ER19BAD"
-        }
-        const response = await request(app)
-        .post("/data")
-        .send(regPlate)
-        
-        expect(response.statusCode).toBe(400)
-        expect(response.body).toEqual(
-            {
-                "errors": [
-                  {
-                    "status": '400',
-                    "code": "400",
-                    "title": "Bad Request",
-                    "detail": "Invalid format for field - vehicle registration number"
-                  }
-                ]
-              }
-        )
+  });
+  test("POST : 404 sends appropriate status and error message when vehicle not found", async () => {
+    const regPlate = "ER19NFD";
+    const response = await request(app).get(
+      `/api/lookup?registrationNumber=${regPlate}`
+    );
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual({
+      errors: [
+        {
+          status: "404",
+          code: "404",
+          title: "Vehicle Not Found",
+          detail: "Record for vehicle not found",
+        },
+      ],
     });
-    test('POST : 404 sends appropriate status and error message when vehicle not found',  async () => {
-        const regPlate = {
-            "registrationNumber": "ER19NFD"
-        }
-        const response = await request(app)
-        .post("/data")
-        .send(regPlate)
-        
-        expect(response.statusCode).toBe(404)
-        expect(response.body).toEqual(
-            {
-                "errors": [
-                  {
-                    "status": "404",
-                    "code": "404",
-                    "title": "Vehicle Not Found",
-                    "detail": "Record for vehicle not found"
-                  }
-                ]
-              }
-              
-        )
-    });
+  });
 });
--- server/index.js
+++ server/index.js
@@ -1,34 +1,41 @@
 const express = require("express");
 const axios = require("axios");
-require('dotenv').config()
-
-const api = process.env.PRODUCTION_API_KEY
 const app = express();
 const cors = require("cors");
+
+
+const ENV = process.env.NODE_ENV || 'development';
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function (req, res, next){
-  res.send("Home Page")
-})
+app.get("/", function (req, res, next) {
+  res.send("Home Page");
+});
 
 app.get("/api/lookup", async (req, res, next) => {
   console.log("Received POST request for /data");
   try {
     const regPlate = req.query.registrationNumber;
+    const apiKey = process.env.NODE_ENV === 'development' ? process.env.DEVELOPMENT_API_KEY : process.env.TEST_API_KEY;
+    const apiUrl = process.env.NODE_ENV === 'development' ? process.env.DEVELOPMENT_URL : process.env.TEST_URL;
     const axiosConfig = {
       method: "post",
-      url: "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles",
+      url: apiUrl,
       headers: {
-        "x-api-key": api,
+        "x-api-key": apiKey,
         "Content-Type": "application/json",
       },
       data: JSON.stringify({ registrationNumber: regPlate }),
     };
 
     const response = await axios(axiosConfig);
-    console.log("Response received from DVLA API", response.data); 
+    console.log("Response received from DVLA API", response.data);
     res.send(response.data);
   } catch (error) {
     console.error("Error processing request", error);
@@ -37,14 +44,12 @@ app.get("/api/lookup", async (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.error("Error handling middleware", err); 
-  if(err.response.status === 400){
-    res.status(400).send(err.response.data)
-  }
-  else if (err.response.status === 404){
-    res.status(404).send(err.response.data)
-  }
-  else res.status(500).send(err.response.data);
+  console.error("Error handling middleware", err);
+  if (err.response.status === 400) {
+    res.status(400).send(err.response.data);
+  } else if (err.response.status === 404) {
+    res.status(404).send(err.response.data);
+  } else res.status(500).send(err.response.data);
 });
 
 module.exports = app;
